7c4af95aeb106137192748167348554e4b700352
SmallConfusion <smallconfusion@protonmail.com>
2024-07-04 17:06:46 -0500

    Removed unnecessary bits and changed 3d camera movement

--- addons/debug_camera/scripts/draggable_camera_2d.gd
+++ addons/debug_camera/scripts/draggable_camera_2d.gd
@@ -1,36 +1,24 @@
 extends Camera2D
-class_name DebugCamera2D
+class_name DraggableCamera2D
 
-# Lower cap for the `_zoom_level`.
-@export var min_zoom := 0.5
-# Upper cap for the `_zoom_level`.
-@export var max_zoom := 2.0
+
 # Controls how much we increase or decrease the `_zoom_level` on every turn of the scroll wheel.
-@export var zoom_factor := 0.1
+@export var zoom_factor := 0.25
 # Duration of the zoom's tween animation.
-@export var zoom_duration := 0.2
+@export var zoom_duration := 0.1
+
 
 # The camera's target zoom level.
 var _zoom_level : float = 1.0 :
 	set(value):
+		_zoom_level = value
 		var tween = get_tree().create_tween()
-		# We limit the value between `min_zoom` and `max_zoom`
-		_zoom_level = clamp(value, min_zoom, max_zoom)
 		tween.tween_property(self, "zoom", Vector2(_zoom_level, _zoom_level), zoom_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
 var _previousPosition: Vector2 = Vector2(0, 0)
 var _moveCamera: bool = false
 
 var main_cam : Camera2D
-
-
-func _ready() -> void:
-	main_cam = get_viewport().get_camera_2d()
-
-
-func _process(_delta: float) -> void:
-	if !enabled:
-		position = main_cam.global_position
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -51,11 +39,3 @@ func _unhandled_input(event: InputEvent) -> void:
 	elif event is InputEventMouseMotion && _moveCamera:
 		position += (_previousPosition - event.position) / _zoom_level
 		_previousPosition = event.position
-	
-	# Toggle cameras
-	if event is InputEventKey && event.is_pressed():
-		if event.keycode == KEY_MINUS:
-			var cam := main_cam
-			cam.enabled = !cam.enabled
-			enabled = !cam.enabled
-
--- addons/debug_camera/scripts/freelook_camera_3d.gd
+++ addons/debug_camera/scripts/freelook_camera_3d.gd
@@ -1,29 +1,15 @@
 extends Camera3D
-class_name DebugCamera3D
+class_name FreelookCamera3D
 
 
 @export_range(0, 10, 0.01) var sensitivity : float = 3
 @export_range(0, 1000, 0.1) var default_velocity : float = 5
 @export_range(0, 10, 0.01) var speed_scale : float = 1.17
 @export_range(1, 100, 0.1) var boost_speed_multiplier : float = 3.0
-@export var max_speed : float = 1000
-@export var min_speed : float = 0.2
-
 @onready var _velocity = default_velocity
-
-var main_cam : Camera3D
-
-
-func _ready() -> void:
-	main_cam = get_viewport().get_camera_3d()
 
 
 func _process(delta: float) -> void:
-	if !current:
-		position = main_cam.global_position
-		rotation = main_cam.global_rotation
-		return
-	
 	var direction = Vector3(
 		float(Input.is_physical_key_pressed(KEY_D)) - float(Input.is_physical_key_pressed(KEY_A)),
 		float(Input.is_physical_key_pressed(KEY_E)) - float(Input.is_physical_key_pressed(KEY_Q)), 
@@ -31,9 +17,9 @@ func _process(delta: float) -> void:
 	).normalized()
 	
 	if Input.is_physical_key_pressed(KEY_SHIFT): # boost
-		translate(direction * _velocity * delta * boost_speed_multiplier)
+		position += direction.rotated(Vector3.UP, rotation.y) * delta * _velocity
 	else:
-		translate(direction * _velocity * delta)
+		position += direction.rotated(Vector3.UP, rotation.y) * delta * _velocity
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -48,14 +34,6 @@ func _unhandled_input(event: InputEvent) -> void:
 			MOUSE_BUTTON_RIGHT:
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
 			MOUSE_BUTTON_WHEEL_UP: # increase fly velocity
-				_velocity = clamp(_velocity * speed_scale, min_speed, max_speed)
+				_velocity *= speed_scale
 			MOUSE_BUTTON_WHEEL_DOWN: # decrease fly velocity
-				_velocity = clamp(_velocity / speed_scale, min_speed, max_speed)
-	
-	# Toggle cameras
-	if event is InputEventKey && event.is_pressed():
-		if event.keycode == KEY_MINUS:
-			var cam := main_cam
-			cam.current = !cam.current
-			current = !cam.current
-
+				_velocity *= speed_scale
