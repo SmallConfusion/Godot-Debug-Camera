372b2ab5a3b5aa303c795890e529494233fe2b12
SmallConfusion <smallconfusion@protonmail.com>
2024-07-04 17:32:11 -0500

    Added throwing to camera2d

--- addons/debug_camera/scripts/draggable_camera_2d.gd
+++ addons/debug_camera/scripts/draggable_camera_2d.gd
@@ -5,20 +5,31 @@ class_name DraggableCamera2D
 # Controls how much we increase or decrease the `_zoom_level` on every turn of the scroll wheel.
 @export var zoom_factor := 0.25
 # Duration of the zoom's tween animation.
-@export var zoom_duration := 0.1
+@export var zoom_duration := 0.2
 
+@export var throw_distance := 8.
+
+@onready var _throw_duration := throw_distance / 50.
+
+var throw_tween : Tween
 
 # The camera's target zoom level.
-var _zoom_level : float = 1.0 :
-	set(value):
-		_zoom_level = value
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "zoom", Vector2(_zoom_level, _zoom_level), zoom_duration).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+var _zoom_level : float = 1.0
 
-var _previousPosition: Vector2 = Vector2(0, 0)
+var _previous_position: Vector2 = Vector2(0, 0)
 var _moveCamera: bool = false
 
-var main_cam : Camera2D
+var _throw_position := Vector2.ZERO
+
+
+func _zoom():
+	get_tree().create_tween().tween_property(self, "zoom", Vector2(_zoom_level, _zoom_level), zoom_duration).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+
+
+func _throw():
+	throw_tween = get_tree().create_tween()
+	throw_tween.tween_property(self, "position", _throw_position + position, _throw_duration).set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+	
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -26,16 +37,24 @@ func _unhandled_input(event: InputEvent) -> void:
 		# zoom out
 		if event.pressed && event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			_zoom_level = _zoom_level - zoom_factor * _zoom_level
+			_zoom()
 		# zoom in
 		if event.pressed && event.button_index == MOUSE_BUTTON_WHEEL_UP:
 			_zoom_level = _zoom_level + zoom_factor * _zoom_level
+			_zoom()
 	
 	if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_RIGHT:
 		if event.is_pressed():
-			_previousPosition = event.position
+			_previous_position = event.position
 			_moveCamera = true
+			if throw_tween:
+				throw_tween.stop()
+				_throw_position = Vector2.ZERO
 		else:
 			_moveCamera = false
+			_throw()
+			
 	elif event is InputEventMouseMotion && _moveCamera:
-		position += (_previousPosition - event.position) / _zoom_level
-		_previousPosition = event.position
+		position += (_previous_position - event.position) / _zoom_level
+		_throw_position = (_previous_position - event.position) * throw_distance
+		_previous_position = event.position
